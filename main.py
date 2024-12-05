736f86b056da4aa2bd04a23e1d061125b7830507
12321k <romankapko7@gmail.com>
2024-12-05 18:58:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QLineEdit
-from PyQt5.QtCore import Qt
+from logica import currency_converter
+
+
+def convert():
+    try:
+        chose = int(choice.text())
+        kurs = int(rate.text())
+        am = int(amount.text())
+    except:
+        print('помилкеа отримання даних')
+    try:
+        result.setText(str(currency_converter(chose, am , kurs)))
+    except:
+        print('виклик функції помилка')
+
 
 app=QApplication([])
 window=QWidget()
-btn=QPushButton("конвертувати")
-lineedit1=QLineEdit()
-lineedit2=QLineEdit()
-lineedit3=QLabel()
 
-lineedit1.setPlaceholderText("Сума в першій валюті")
-lineedit2.setPlaceholderText("курс")
+btn=QPushButton("конвертувати")
+amount=QLineEdit()
+rate=QLineEdit()
+choice=QLineEdit()
+result=QLabel()
+
+amount.setPlaceholderText("Сума для конвертації")
+rate.setPlaceholderText("Курс валют")
+choice.setPlaceholderText("Напрямок конвертації (1 для UAH -> USD, 2 для USD -> UAH)v")
 
 v_line = QVBoxLayout()
 
-v_line.addWidget(lineedit1)
-v_line.addWidget(lineedit2)
-v_line.addWidget(lineedit3)
+v_line.addWidget(amount)
+v_line.addWidget(rate)
+v_line.addWidget(choice)
+v_line.addWidget(result)
 v_line.addWidget(btn)
 window.setLayout(v_line)
+
+btn.clicked.connect(convert)
+
 
 window.show()
 app.exec_()
