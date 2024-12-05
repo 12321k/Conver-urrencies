3be2b65cb9be2cd7392a8e371eeb8e047c580474
12321k <romankapko7@gmail.com>
2024-12-05 18:56:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ def currency_converter(choice, amount, rate):
     :param rate: Курс валют
     :return: Конвертована сума
     """
-    if choice == 1:
-        result = amount / rate
-    elif choice == 2:
-        result = amount * rate
-    else:
-        print("Невірний вибір. Будь ласка, виберіть 1 або 2.")
+    try:
+        if choice == 1:
+            result = amount / rate
+        elif choice == 2:
+            result = amount * rate
+        else:
+            print("Невірний вибір. Будь ласка, виберіть 1 або 2.")
+            return None
+    except:
+        print('Ділення на нуль')
         return None
     return result
